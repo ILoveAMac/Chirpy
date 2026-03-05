45e9d895a38945d29ee2f1589e013e4bcc1340af
wihan <wihan@asymptote.co.za>
2026-03-05 20:55:16 +0200

    Server health check

--- main.go
+++ main.go
@@ -12,8 +12,17 @@ func main() {
 	}
 	server.Addr = ":8080"
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServer := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+
+	mux.HandleFunc("/healthz", serverHealth)
 
 	// Start the server
 	server.ListenAndServe()
+}
+
+func serverHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
